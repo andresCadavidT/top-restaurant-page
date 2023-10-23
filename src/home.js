99216f6bae4bb5178f1ebd4ad8c6a6408ecb75e5
Andres Correa <andrescorreac@gmail.com>
2023-10-23 13:37:29 -0500

    Add js using webpack

---
+++ src/home.js
@@ -0,0 +1,22 @@
+export default function home() {
+    const h2 = document.createElement("h2")
+    h2.textContent = "Sabores Deliciosos, Restaurante"
+    const homeChef = document.createElement("img")
+    homeChef.src = "../dist/home-chef.jpg"
+    homeChef.className = "homeChef"
+    const p = document.createElement("p")
+    p.textContent = "¡Bienvenido a 'Sabores Deliciosos'! Situado en el corazón de la ciudad, somos conocidos por deleitar a nuestros comensales con una experiencia culinaria única. Nuestro chef galardonado combina ingredientes frescos y técnicas innovadoras para crear platos que deleitan los sentidos. Desde nuestras exquisitas entradas hasta nuestros postres irresistibles, cada bocado es una explosión de sabores. Nuestro ambiente acogedor y servicio excepcional hacen que cada visita sea memorable. Únete a nosotros y descubre por qué 'Sabores Deliciosos' es el destino preferido para los amantes de la buena comida en la ciudad."
+
+
+    const divHome = document.createElement("div")
+    divHome.classList = "divHome"
+    divHome.appendChild(h2)
+    divHome.appendChild(homeChef)
+    divHome.appendChild(p)
+    const contentDivDOM = document.querySelector("#content")
+    contentDivDOM.appendChild(divHome)
+}
+
+
+
+// Foto de <a href="https://unsplash.com/es/@lvnatikk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Lily Banse</a> en <a href="https://unsplash.com/es/fotos/plato-cocinado-en-tazon-gris--YHSwy6uqvk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-import initialPageLoad from "./initial-page-load.js"
+import home from "./home.js"
+import menu from "./menu.js"
+import contact from "./contact.js"
 
-console.log("Just Checking")
-initialPageLoad()
+function clearDivDOM(){
+    const contentDivDOM = document.querySelector("#content")
+    while (contentDivDOM.firstChild) {
+        contentDivDOM.removeChild(contentDivDOM.firstChild)
+    }
+}
+
+home()
+
+const navHome = document.querySelector(".navHome")
+navHome.addEventListener("click", ()=>{
+    clearDivDOM()
+    home()
+})
+
+const navMenu = document.querySelector(".navMenu")
+navMenu.addEventListener("click", ()=>{
+    clearDivDOM()
+    menu()
+})
+
+const navContact = document.querySelector(".navContact")
+navContact.addEventListener("click", ()=>{
+    clearDivDOM()
+    contact()
+})
+
+
+
